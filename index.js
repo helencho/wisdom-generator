62f832e37325147160f201e4033b88a64d645e87
helencho <helencho@ac.c4q.nyc>
2017-12-07 18:14:54 -0500

    updated stylesheet

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 let peter = 'https://raw.githubusercontent.com/helencho/wisdom-generator/master/peter.json';
 let backgrounds = 'https://raw.githubusercontent.com/helencho/wisdom-generator/master/backgrounds.json';
-let text
+let text;
+
 document.addEventListener('DOMContentLoaded', () => {
     let quotediv = document.querySelector('#quote');
     let body = document.querySelector('body');
-    fetch(peter)
+
+
+    fetch(backgrounds)
         .then((res) => {
             return res.json();
-        }).then((quoteArray) => {
-            fetch(backgrounds)
+        }).then((bgArray) => {
+            let bg = random(bgArray);
+            body.style.backgroundImage = `url(${bg})`;
+            body.style.backgroundSize = 'cover';
+            body.style.height = '100vh';
+            body.style.backgroundRepeat = 'no-repeat';
+
+            fetch(peter)
                 .then((res) => {
                     return res.json();
-                }).then((bgArray) => {
-                    let bg = random(bgArray);
-                    body.style.backgroundImage = `url(${bg})`;
-                    body.style.backgroundSize = 'cover';
-                    body.style.height = '100vh';
-                    body.style.backgroundRepeat = 'no-repeat';
+                }).then((quoteArray) => {
                     text = random(quoteArray)
                 })
-        })
+        });
+
+
+    // fetch(peter)
+    //     .then((res) => {
+    //         return res.json();
+    //     }).then((quoteArray) => {
+    //         fetch(backgrounds)
+    //             .then((res) => {
+    //                 return res.json();
+    //             }).then((bgArray) => {
+    //                 let bg = random(bgArray);
+    //                 body.style.backgroundImage = `url(${bg})`;
+    //                 body.style.backgroundSize = 'cover';
+    //                 body.style.height = '100vh';
+    //                 body.style.backgroundRepeat = 'no-repeat';
+    //                 text = random(quoteArray)
+    //             })
+    //     });
+
+    document.addEventListener("click", () => reload());
+
 });
 
 // randomly chooses an element from an array 
@@ -27,32 +52,30 @@ const random = (array) => {
     return array[Math.floor(Math.random() * (array.length))];
 }
 
-
-var $ = function(id) {
+var $ = function (id) {
     return document.getElementById(id);
-  };
-  var inc = 0;
-  var out = 0;
-  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
-  var timer;
-  
-  var anim = function() {
+};
+
+var inc = 0;
+var out = 0;
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+var timer;
+
+var anim = function () {
     inc++;
     if (inc % 7 === 0 && out < text.length) {
-      $('anim').appendChild(document.createTextNode(text[out]));
-      out++;
+        $('anim').appendChild(document.createTextNode(text[out]));
+        out++;
 
     }
-  };
-  timer = setInterval(anim, 4);
-  $('anim').innerHTML = '';
-  
-  function reload() {
+};
+
+timer = setInterval(anim, 4);
+$('anim').innerHTML = '';
+
+function reload() {
     window.location.href = window.location.href;
-  }
-
-document.addEventListener("click", () => reload());
-
+}
 
 
 
