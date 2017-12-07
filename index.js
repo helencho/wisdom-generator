dc781be18ae658a6cef623e15b1fc14e228e6db9
David Shin <32101386+davidyshin@users.noreply.github.com>
2017-12-07 16:15:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let peter = 'https://raw.githubusercontent.com/helencho/wisdom-generator/master/peter.json';
 let backgrounds = 'https://raw.githubusercontent.com/helencho/wisdom-generator/master/backgrounds.json';
-
+let text
 document.addEventListener('DOMContentLoaded', () => {
     let quotediv = document.querySelector('#quote');
     let body = document.querySelector('body');
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     body.style.backgroundSize = 'cover';
                     body.style.height = '100vh';
                     body.style.backgroundRepeat = 'no-repeat';
-                    quotediv.innerText = random(quoteArray);
+                    text = random(quoteArray)
                 })
         })
 });
@@ -26,3 +26,29 @@ document.addEventListener('DOMContentLoaded', () => {
 const random = (array) => {
     return array[Math.floor(Math.random() * (array.length))];
 }
+
+
+var $ = function(id) {
+    return document.getElementById(id);
+  };
+  var inc = 0;
+  var out = 0;
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+  var timer;
+  
+  var anim = function() {
+    inc++;
+    if (inc % 7 === 0 && out < text.length) {
+      $('anim').appendChild(document.createTextNode(text[out]));
+      out++;
+
+    }
+  };
+  timer = setInterval(anim, 4);
+  $('anim').innerHTML = '';
+  
+  function reload() {
+    window.location.href = window.location.href;
+  }
+
+document.addEventListener("click", () => reload());
